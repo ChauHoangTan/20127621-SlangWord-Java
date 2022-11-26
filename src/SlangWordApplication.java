32517f0e0e3a1b8cfb984e47837c7a07f4081405
ChauHoangTan <20127621@student.hcmus.edu.vn>
2022-11-26 20:40:28 +0700

    update file application using swing

--- src/SlangWordApplication.java
+++ src/SlangWordApplication.java
@@ -86,9 +86,119 @@ public class SlangWordApplication extends JPanel {
         centerPanel1.add(centerPanel2);
         //
 
+        // create table from file data.txt
+
+        JPanel centerPanel3 = new JPanel();
+        centerPanel3.setLayout(new BoxLayout(centerPanel3, BoxLayout.Y_AXIS));
+
+
+        String[] columnNames = {"STT","SLang Word", "Definition"};
+        String data[][] ={{"1","2","3"}};
+
+
+        //create table with data
+
+        JTable listSlangWord = new JTable(data,columnNames);
+
+        listSlangWord.getColumnModel().getColumn(0).setPreferredWidth(50);
+        listSlangWord.getColumnModel().getColumn(1).setPreferredWidth(200);
+        listSlangWord.getColumnModel().getColumn(2).setPreferredWidth(200);
+
+        JScrollPane scrollPane = new JScrollPane(listSlangWord);
+
+        // create 2 button reset and random
+
+        JButton btnReset = new JButton("Reset");
+        JButton btnRandom = new JButton("Random");
+
+        JPanel centerPanel31 = new JPanel();
+        centerPanel31.setLayout(new FlowLayout());
+
+        //JScrollPane scrollPane = new JScrollPane(listSlangWord);
+//        listSlangWord.setFillsViewportHeight(true);
+
+        centerPanel31.add(btnReset);
+        centerPanel31.add(Box.createRigidArea(new Dimension(20,10)));
+        centerPanel31.add(btnRandom);
+
+        centerPanel31.setBorder(BorderFactory.createLineBorder(Color.cyan));
+
+        centerPanel3.add(scrollPane);
+        centerPanel3.add(centerPanel31);
+
+        centerPanel3.setPreferredSize(new Dimension(550, 200));
+        centerPanel3.setMaximumSize(new Dimension(550, 300));  // hardCoded sizing
+        centerPanel3.setMinimumSize(new Dimension(550, 300));  // hardCoded sizing
+
+        centerPanel1.add(centerPanel3);
+
+
+
+        // create bot panel
+        JPanel botPanel = new JPanel();
+        botPanel.setLayout(new BoxLayout(botPanel, BoxLayout.Y_AXIS));
+
+
+        // creaye label đố vui mà combobox chọn câu hỏi tìm slang word hoặc tìm definition
+        JPanel botPanel1 = new JPanel();
+        botPanel1.setLayout(new FlowLayout());
+        JLabel labelQuiz = new JLabel("Đố vui ");
+        String listOptionQuiz[] = {"Tìm Slang word", "Tìm Definition"};
+        JComboBox cbbQuiz = new JComboBox(listOptionQuiz);
+        botPanel1.add(labelQuiz);
+        botPanel1.add(Box.createRigidArea(new Dimension(20,0)));
+        botPanel1.add(cbbQuiz);
+
+        // tạo label question và câu hỏi
+        JPanel botPanel2 = new JPanel();
+        botPanel2.setLayout(new FlowLayout());
+        JLabel labelQuestion = new JLabel("Question : ");
+        JLabel textQuestion = new JLabel("My name is ?");
+        botPanel2.add(labelQuestion);
+        botPanel2.add(Box.createRigidArea(new Dimension(20,0)));
+        botPanel2.add(textQuestion);
+
+        // tạo 4 đáp án cho người dùng chọn
+        JPanel botPanel3 = new JPanel(new GridLayout(2,2));
+
+        JRadioButton answer1 = new JRadioButton("A");
+        JRadioButton answer2 = new JRadioButton("B");
+        JRadioButton answer3 = new JRadioButton("C");
+        JRadioButton answer4 = new JRadioButton("D");
+
+        botPanel3.add(answer1);
+        botPanel3.add(answer2);
+        botPanel3.add(answer3);
+        botPanel3.add(answer4);
+
+        botPanel3.setPreferredSize(new Dimension(360, 200));
+        botPanel3.setMaximumSize(new Dimension(360, 300));  // hardCoded sizing
+        botPanel3.setMinimumSize(new Dimension(360, 300));  // hardCoded sizing
+
+
+        // tạo nút next để qua câu hỏi khác, nút answer để xem đáp án, nút stop để kết thúc
+        JPanel botPanel4 = new JPanel(new FlowLayout());
+        JButton btnShowAnswer = new JButton("Show answer ");
+        JButton btnStop = new JButton("Stop");
+        JButton btnNext = new JButton("Next");
+
+        botPanel4.add(btnShowAnswer);
+        botPanel4.add(Box.createRigidArea(new Dimension(20,10)));
+        botPanel4.add(btnStop);
+        botPanel4.add(Box.createRigidArea(new Dimension(20,10)));
+        botPanel4.add(btnNext);
+
+
+        // add 4 panel vào bot panel
+        botPanel.add(botPanel1);
+        botPanel.add(botPanel2);
+        botPanel.add(botPanel3);
+        botPanel.add(botPanel4);
 
         add(topPanel, BorderLayout.NORTH);
         add(centerPanel1, BorderLayout.CENTER);
+        add(botPanel, BorderLayout.PAGE_END);
+
 
     }
 
@@ -104,6 +214,8 @@ public class SlangWordApplication extends JPanel {
 
         frame.setVisible(true);
         frame.pack();
+
+
     }
 
     public static void main(String []args){
